2b53bb3deb19ce576f9a2a6889777a0f5b5d3c83
soregashi-27 <tomo.soul.15@gmail.com>
2021-04-08 17:41:37 +0900

    nodejsTraining

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('Hello World from node.js');
+
+const tutorial = require('./tutorial');
+console.log(tutorial);
+console.log(tutorial.sum(1, 1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
